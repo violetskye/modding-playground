408333bae0d59a7fd0048a5cdc882a435ee68ded
violetskye <violetskye@users.noreply.github.com>
2015-03-08 18:17:15 -0700

    Added a new item.

--- main/java/com/starfishsparkle/myfirstmod/MyFirstMod.java
+++ main/java/com/starfishsparkle/myfirstmod/MyFirstMod.java
@@ -4,6 +4,7 @@ package com.starfishsparkle.myfirstmod;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Metadata;
@@ -16,20 +17,29 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class MyFirstMod
 {
     public static final String MODID = "My Sparkly Mod";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.3";
+    public static final String ITEM_NAME = "Best Item. Not!";
     
     @Metadata
     public static ModMetadata meta;
     
     public static Block myFirstBlock;
+    public static Item myFirstItem; 
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+    	// blockode
     	myFirstBlock = new MyFirstBlock()
     		.setBlockName("My First Block")
     		.setCreativeTab(CreativeTabs.tabBlock);
     	GameRegistry.registerBlock(myFirstBlock, "My First Block");
+    	
+    	// Itemkode 
+    	myFirstItem = new Item()
+    		.setUnlocalizedName(ITEM_NAME)
+    		.setCreativeTab(CreativeTabs.tabMisc);
+    	GameRegistry.registerItem(myFirstItem, ITEM_NAME);
     }
     
     @EventHandler
