2415aa84c967f339bcf0ac93463e96298a3a331d
violetskye <violetskye@users.noreply.github.com>
2015-04-12 17:24:45 -0700

    added names
    “fuzzy wuzzy” and “best item. not!” are here to have fun!!!!!!!!

--- main/java/com/starfishsparkle/myfirstmod/MyFirstMod.java
+++ main/java/com/starfishsparkle/myfirstmod/MyFirstMod.java
@@ -19,7 +19,7 @@ public class MyFirstMod
 {
     public static final String MODID = "My Sparkly Mod";
     public static final String VERSION = "0.3";
-    public static final String ITEM_NAME = "Best Item. Not!";
+    public static final String ITEM_NAME = "Best Item Not";
     
     @Metadata
     public static ModMetadata meta;
